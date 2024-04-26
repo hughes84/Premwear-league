2dece3db3a7ff817f6e865499b235c01f240f76b
hughes84 <134289376+hughes84@users.noreply.github.com>
2024-04-26 18:41:19 +0100

    Add best selling products carousel to homepage

--- home/views.py
+++ home/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from django.db.models import Avg
+from products.models import Product
 
-# Create your views here.
+# pylint: disable=no-member
 
 def index(request):
     """ A view to return the index page """
 
-    return render(request, 'home/index.html')
+     # Query top-rated products with average rating greater than or equal to 3
+    top_rated_products = Product.objects.annotate(
+        avg_rating=Avg('rating')
+    ).filter(avg_rating__gte=3)
+    return render(request, 'home/index.html', {'rated': top_rated_products})
+
